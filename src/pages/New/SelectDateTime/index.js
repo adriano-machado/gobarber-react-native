e037aa8c5556947cd6544e735200d75565606fa8
Adriano Machado <adrianoricardom@gmail.com>
2020-04-12 15:16:47 -0300

    add: datepicker to android and IOS

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
 // import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+  return (
+    <Background>
+      <DateInput date={date} onChange={setDate} />
+    </Background>
+  );
 }
